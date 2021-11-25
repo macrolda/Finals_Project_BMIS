49b737653aa109f7c64d27530394e35c7041ecfa
Maria Ailah Rolda <rolda.mariaailah@ue.edu.ph>
2021-11-25 09:08:19 +0800

    added resident table

--- AdminResidentRecords.php
+++ AdminResidentRecords.php
@@ -1,129 +1,314 @@
 <?php
     session_start();
     require ("config.php");
-    include'AdminSidebar.php';
 ?>
 
+<!DOCTYPE HTML>
+<html lang="en">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<link rel="icon" type="image/png" sizes="32x32" href="image/logo_1.png">
 <head>
     <title>Admin | Resident Records</title>
-    <link type="text/css" href="css/AdminResidentRecords.css" rel="stylesheet">             
+    <link type="text/css" href="css/AdminResidentRecords.css" rel="stylesheet">
 </head>
 
 <body>
-    <div class="row">
-        <div class="col-md-3 "></div>
-
-        <div class="col-md-auto mainPage" >
-            <h3 style="text-align: left; margin-bottom: 30px; font-size: 30px">Resident Records</h3>
-            <! -- cards -->
-            <div class="cardBox">
-                <div class="cardd">
-                    <div class="col-md-auto"><h2>Resident's Details</h2></div>
+    <div class="container">
+        <!-- START OF SIDE BAR -->
+        <div class="navigation ">
+            <ul>
+                <div>
+                    <div class="user">
+                        <img class="imgLogo" src="image/logo_1.png">
+                    </div>
+                    <h1 class="brgyName">Barangay Tabunok</h1>
                 </div>
-                <div class="cardd">
-                    <div class="col-md-auto">
-                        <button type="button" class="btn btn-info add-new"><i class="fa fa-plus"></i> Add New</button>
+
+                <li class="list ">
+                    <a href="AdminDashboard.php">
+                        <span class="icon">
+                            <ion-icon name="home-outline"><ion-icon>    
+                        </span>
+                        <span class="title">Dashboard</span>
+                    </a>
+                </li>
+
+                <li class="list hovered">
+                    <a href="AdminResidentRecords.php">
+                        <span class="icon">
+                            <ion-icon name="person-outline"><ion-icon>    
+                        </span>
+                        <span class="title">Resident Records</span>
+                    </a>
+                </li>
+
+                <li class="list">
+                    <a href="#">
+                        <span class="icon">
+                            <ion-icon name="briefcase-outline"><ion-icon>    
+                        </span>
+                        <span class="title">Blotter Records</span>
+                    </a>
+                </li>
+            
+                <li class="list">
+                    <a href="#">
+                        <span class="icon">
+                            <ion-icon name="calendar-outline"><ion-icon>    
+                        </span>
+                        <span class="title">Settlement Schedules</span>
+                    </a>
+                </li>
+
+                <li class="list">
+                    <a href="#">
+                        <span class="icon">
+                            <ion-icon name="print-outline"><ion-icon>    
+                        </span>
+                        <span class="title">Certificate Records</span>
+                    </a>
+                </li>
+
+                <li class="list">
+                    <a href="#">
+                        <span class="icon">
+                            <ion-icon name="settings-outline"><ion-icon>    
+                        </span>
+                        <span class="title">Barangay Settings</span>
+                    </a>
+                </li>
+
+                <li class="list">
+                    <a href="index.php">
+                        <span class="icon">
+                            <ion-icon name="log-out-outline"><ion-icon>    
+                        </span>
+                        <span class="title">Log-Out</span>
+                    </a>
+                </li>
+
+                <div  class="currentDayTime">
+                    <p>Date/Time: <br><span id="datetime"></span></p>
+                </div>
+            </ul>
+        </div>
+        <!-- END OF SIDE BAR -->
+     
+        <div class="main">
+            <div class="topbar">
+                <!-- userImg -->
+                <div class="user">
+                    <img  src="image/default_img.png">
+                </div>
+            </div>
+            
+            <div class="col-md-auto mainPage" >
+                <h3 
+                style="text-align: 
+                center; margin-bottom: 
+                30px; font-size: 30px">Residents Records</h3>
+                <! -- TABLE -->
+                <div class="container-xl">
+                    <div class="table-responsive">
+                        <div class="table-wrapper">
+			                <div class="table-title">
+				                <div class="row">
+					                <div class="col-sm-6">
+						                <h2>Resident's <b>Details</b></h2>
+					                </div>
+					                <div class="col-sm-6">
+						                <a href="#addEmployeeModal" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i> <span>Add New Employee</span></a>
+						                <a href="#deleteEmployeeModal" class="btn btn-danger" data-toggle="modal"><i class="material-icons">&#xE15C;</i> <span>Delete</span></a>						
+					                </div>
+				                </div>
+			                </div>
+                            
+			                <table class="table table-striped table-hover">
+				                <thead>
+					                <tr>
+						                <th>
+							                <span class="custom-checkbox">
+								                <input type="checkbox" id="selectAll">
+								                <label for="selectAll"></label>
+							                </span>
+						                </th>
+                                        <th>ResidentID</th>
+						                <th>FirstName</th>
+						                <th>MiddleName</th>
+						                <th>LastName</th>
+                                        <th>Sex</th>
+						                <th>BirthDate</th>
+						                <th>BirthPlace</th>
+						                <th>ContactNumber</th>
+                                        <th>Email</th>
+                                        <th>Address</th>
+						                <th>RegisteredVoter</th>
+						                <th>Actions</th>
+					                </tr>
+				                </thead>
+				                <tbody>
+					                <tr>
+						                <td>
+							                <span class="custom-checkbox">
+								                <input type="checkbox" id="checkbox1" name="options[]" value="1">
+								                <label for="checkbox1"></label>
+							                </span>
+						                </td>
+                                        <td>ResidentID</td>
+						                <td>FirstName</td>
+						                <td>MiddleName</td>
+						                <td>LastName</td>
+                                        <td>Sex</td>
+						                <td>BirthDate</td>
+						                <td>BirthPlace</td>
+						                <td>ContactNumber</td>
+                                        <td>Email</td>
+                                        <td>Address</td>
+						                <td>RegisteredVoter</td>
+						                    <td>
+							                    <a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
+							                    <a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
+						                    </td>
+					                </tr>					
+				                </tbody>
+			                </table>
+			                <div class="clearfix">
+				                <div class="hint-text">Showing <b>5</b> out of <b>25</b> entries</div>
+				                    <ul class="pagination">
+					                    <li class="page-item disabled"><a href="#">Previous</a></li>
+					                    <li class="page-item active"><a href="#" class="page-link">1</a></li>
+					                    <li class="page-item"><a href="#" class="page-link">2</a></li>
+					                    <li class="page-item"><a href="#" class="page-link">3</a></li>
+					                    <li class="page-item"><a href="#" class="page-link">4</a></li>
+					                    <li class="page-item"><a href="#" class="page-link">5</a></li>
+					                    <li class="page-item"><a href="#" class="page-link">Next</a></li>
+				                    </ul>
+			                    </div>
+		                    </div>
+	                    </div>        
+                    </div>
+                    <!-- Edit Modal HTML -->
+                    <div id="addEmployeeModal" class="modal fade">
+	                    <div class="modal-dialog">
+		                    <div class="modal-content">
+			                    <form>
+				                    <div class="modal-header">						
+					                    <h4 class="modal-title">Add Employee</h4>
+					                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				                    </div>
+				                    <div class="modal-body">					
+					                    <div class="form-group">
+						                    <label>Name</label>
+						                    <input type="text" class="form-control" required>
+					                    </div>
+					                    <div class="form-group">
+						                    <label>Email</label>
+						                    <input type="email" class="form-control" required>
+					                    </div>
+					                    <div class="form-group">
+						                    <label>Address</label>
+						                    <textarea class="form-control" required></textarea>
+					                    </div>
+					                    <div class="form-group">
+						                    <label>Phone</label>
+						                    <input type="text" class="form-control" required>
+					                    </div>					
+				                    </div>
+				                    <div class="modal-footer">
+					                    <input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
+					                    <input type="submit" class="btn btn-success" value="Add">
+				                    </div>
+			                    </form>
+		                    </div>
+	                    </div>
+                    </div>
+                    <!-- Delete Modal HTML -->
+                    <div id="deleteEmployeeModal" class="modal fade">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <form>
+                                    <div class="modal-header">						
+					                    <h4 class="modal-title">Delete Employee</h4>
+					                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				                    </div>
+                                    <div class="modal-body">					
+					                    <p>Are you sure you want to delete these Records?</p>
+					                    <p class="text-warning"><small>This action cannot be undone.</small></p>
+				                    </div>
+				                    <div class="modal-footer">
+					                    <input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
+					                    <input type="submit" class="btn btn-danger" value="Delete">
+				                    </div>
+                                    <div class="modal-body">					
+					                    <p>Are you sure you want to delete these Records?</p>
+					                    <p class="text-warning"><small>This action cannot be undone.</small></p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
+                                        <input type="submit" class="btn btn-danger" value="Delete">
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                
-
-                <table class="table table-bordered">
-                    <thead>
-                        <tr>
-                            <th>Resident ID</th>
-                            <th>FirstName</th>
-                            <th>MiddleName</th>
-                            <th>LastName</th>
-                            <th>Gender</th>
-                            <th>BirthDate</th>
-                            <th>BirthPlace</th>
-                            <th>CivilStatus</th>
-                            <th>VoterStatus</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>127</td>
-                            <td>John</td>
-                            <td>Joe</td>
-                            <td>Doe</td>
-                            <td>Male</td>
-                            <td>12/0/98</td>
-                            <td>Single</td>
-                            <td>Manila</td>
-                            <td>Yes</td>
-                        
-                            <td>
-							    <a class="add" title="Add" data-toggle="tooltip"><i class="material-icons">&#xE03B;</i></a>
-                                <a class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a>
-                                <a class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE872;</i></a>
-                            </td>
-                        </tr>    
-                    </tbody>
-                </table>  
-            </div>    
-            
+            </div>
         </div>
     </div>
+<!---SCRIPTS--->
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+<script>
+    //Date and timestamp
+    var x = new Date()
+    var x1= x.getMonth() + 1+ "/" + x.getDate() + "/" + x.getFullYear() + " - " +  x.getHours( )+ ":" +  x.getMinutes() + ":" +  x.getSeconds();
+    document.getElementById('datetime').innerHTML = x1;
+    
+    // add hovered class in selected list item
+    let list = document.querySelectorAll('.navigation li');
+    function activeLink(){
+        list.forEach((item) => item.classList.remove('hovered'));
+        this.classList.add('hovered');
+    }
+    list.forEach((item) => item.addEventListener('mouseover',activeLink));
 
-    <script>
-$(document).ready(function(){
+    //modal
+    $(document).ready(function(){
+	// Activate tooltip
 	$('[data-toggle="tooltip"]').tooltip();
-	var actions = $("table td:last-child").html();
-	// Append table with add row form on add new button click
-    $(".add-new").click(function(){
-		$(this).attr("disabled", "disabled");
-		var index = $("table tbody tr:last-child").index();
-        var row = '<tr>' +
-            '<td><input type="text" class="form-control" name="Resident ID" id="residentID"></td>' +
-            '<td><input type="text" class="form-control" name="FirstName" id="FirstName"></td>' +
-            '<td><input type="text" class="form-control" name="MiddleName" id="MiddleName"></td>' +
-            '<td><input type="text" class="form-control" name="Resident ID" id="LastName"></td>' +
-            '<td><input type="text" class="form-control" name="FirstName" id="Gender"></td>' +
-            '<td><input type="text" class="form-control" name="MiddleName" id="BirthDate"></td>' +
-            '<td><input type="text" class="form-control" name="Resident ID" id="BirthPlace"></td>' +
-            '<td><input type="text" class="form-control" name="FirstName" id="CivilStatus"></td>' +
-            '<td><input type="text" class="form-control" name="VoterStatus" id="VoterStatus"></td>' +
-			'<td>' + actions + '</td>' +
-        '</tr>';
-    	$("table").append(row);		
-		$("table tbody tr").eq(index + 1).find(".add, .edit").toggle();
-        $('[data-toggle="tooltip"]').tooltip();
-    });
-	// Add row on add button click
-	$(document).on("click", ".add", function(){
-		var empty = false;
-		var input = $(this).parents("tr").find('input[type="text"]');
-        input.each(function(){
-			if(!$(this).val()){
-				$(this).addClass("error");
-				empty = true;
-			} else{
-                $(this).removeClass("error");
-            }
-		});
-		$(this).parents("tr").find(".error").first().focus();
-		if(!empty){
-			input.each(function(){
-				$(this).parent("td").html($(this).val());
-			});			
-			$(this).parents("tr").find(".add, .edit").toggle();
-			$(".add-new").removeAttr("disabled");
-		}		
-    });
-	// Edit row on edit button click
-	$(document).on("click", ".edit", function(){		
-        $(this).parents("tr").find("td:not(:last-child)").each(function(){
-			$(this).html('<input type="text" class="form-control" value="' + $(this).text() + '">');
-		});		
-		$(this).parents("tr").find(".add, .edit").toggle();
-		$(".add-new").attr("disabled", "disabled");
-    });
-	// Delete row on delete button click
-	$(document).on("click", ".delete", function(){
-        $(this).parents("tr").remove();
-		$(".add-new").removeAttr("disabled");
-    });
+	
+	// Select/Deselect checkboxes
+	var checkbox = $('table tbody input[type="checkbox"]');
+	$("#selectAll").click(function(){
+		if(this.checked){
+			checkbox.each(function(){
+				this.checked = true;                        
+			});
+		} else{
+			checkbox.each(function(){
+				this.checked = false;                        
+			});
+		} 
+	});
+	checkbox.click(function(){
+		if(!this.checked){
+			$("#selectAll").prop("checked", false);
+		}
+	});
 });
 </script>
+<!---SCRIPTS--->
 </body>
+</html>
