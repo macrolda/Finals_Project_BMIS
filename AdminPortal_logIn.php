a077b08a8caeec6c55de93d2ec9a4cb0f943e6c6
Maria Ailah Rolda <rolda.mariaailah@ue.edu.ph>
2021-11-28 00:10:25 +0800

    added resident registration

--- AdminPortal_logIn.php
+++ AdminPortal_logIn.php
@@ -10,7 +10,7 @@ require ("config.php");
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <link rel="icon" type="image/png" sizes="32x32" href="image/logo_1.png">
 <head>
-    <title>Administrator's LogIn</title>
+    <title>Administrator | Log-In</title>
     <link type="text/css" href="css/AdminPortalLogIn.css" rel="stylesheet">
 </head>
 <body>
--- ResidentRegister.php
+++ ResidentRegister.php
@@ -8,21 +8,16 @@ require ("config.php");
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <link rel="icon" type="image/png" sizes="32x32" href="image/logo_1.png">
 <head>
-    <title>Resident | Log-In</title>
-    <link type="text/css" href="css/ResidentPortalLogIn.css" rel="stylesheet">
+    <title>Resident | Register</title>
+    <link type="text/css" href="css/ResidentRegister.css" rel="stylesheet">
 </head>
 <body>
-<div class="login-wrapper">
+    <div class="login-wrapper">
         <form action="" class="form">
             <img src="image\logo_1.png" alt="barangayLogo1">
-            <h2>Resident's Log-In</h2>
+            <h2>Resident's Registration</h2>
             <div class="input-group">
                 <input 
                 type="text" 
@@ -34,29 +29,27 @@ require ("config.php");
 
             <div class="input-group">
                 <input 
+                type="email" 
+                name="emailUser" 
+                id="emailUser" 
+                required>
+                <label for="emailUser">Email</label>
+            </div>
+
+            <div class="input-group">
+                <input 
                 type="password" 
                 name="loginPassword" 
                 id="loginPassword" 
                 required>
                 <label for="loginPassword">Password</label>
             </div>
-            
-            <a href="#forgot-pw" class="forgot-pw">Forgot Password?</a>
-            
+
             <input 
             type="submit"
-            value="Login" 
+            value="Register" 
             class="submit-btn" 
-            onclick="location='ResidentDashboard.php'">
-            
-            <hr size = 2 noshade color=white>
-
-            <div class="registerTxt">
-                <p>Don't have an account? <a href="ResidentRegister.php">Register here</a></p>
-            </div>
-
-            
-
+            onclick="location='ResidentPortal_logIn.php'">
         </form>
 
         <div id="forgot-pw">
@@ -69,7 +62,7 @@ require ("config.php");
                 </div>
                 <div class="input-group">
                     <input type="password" name="newPassword" id="newPassword" required>
-                    <label for="newPassword">New Password</label>
+                    <label for="newPassword">Password</label>
                 </div>
                 <div class="input-group">
                     <input type="password" name="confirmPassword" id="confirmPassword" required>
